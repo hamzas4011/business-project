e4741a62deb87fd4bf9bafcd29c7c7c3e4a917c8
Hamza Simsek <hamzas4011@hotmail.com>
2023-10-24 23:12:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 
-"""
-This program shows a business management system by using two classes.
-Business class represent business name, money available and dictionary to store items and
-deals. Ware class has items with name and price. Furthermore, this Python program allows
-the user to create a new business or manage existing ones, restocking items by decreasing available money
-and increasing item quantities, checking item prices with deals, checking item availability,
-sell items, add and remove deals, and quit the program.
-
-So why did we use the pickle module?
-The dictionary holding details about the business are stored in a file using pickle,
-then it allows the program to load and save data between sessions.
-"""
 import pickle
 
 # Ware class to store information about ware
